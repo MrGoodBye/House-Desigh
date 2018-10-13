9c38f20a429b0a78dc0087cb177cb9e4f2229276
MrGoodBye <1254993267@qq.com>
2018-10-13 18:34:07 +0800

    dynamic of service items

--- src/service/index.js
+++ src/service/index.js
@@ -1,7 +1,23 @@
 import './index.scss'
+import CONSTS from './consts'
 
 const isMobile = window.isMobile
-$('.service-item-wrapper').slick({
+const $serviceContainer = $('.service-item-wrapper')
+const images = window.importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/))
+$serviceContainer.html(() => images.map((image, i) => `
+    <div
+        class="service-item"
+        data-aos=${isMobile ? "fade-in" : "fade-up"}
+        data-aos-delay=${i * 100}
+    >
+        <img src=${image} alt=${CONSTS[i].title}>
+        <div class="service_info">
+            <p class="title">${CONSTS[i].title}</p>
+            <p class="description">${CONSTS[i].description}</p>
+        </div>
+    </div>
+`))
+$serviceContainer.slick({
     infinite: true,
     speed: 300,
     slidesToShow: isMobile ? 1 : 3,
