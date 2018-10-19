c2cc3d871368dd61d31faf57f4d64dca9673b444
MrGoodBye <1254993267@qq.com>
2018-10-19 21:26:59 +0800

    remove the external link temproary

--- src/team/consts.js
+++ src/team/consts.js
@@ -3,24 +3,28 @@ export default {
         title: '乔纳森 / Joseph',
         subtitle: '运营总监/PM',
         description: '设计界的“设计鬼才”从题材到表现方式都引起了极大的福布斯中国最具潜力 ...',
-        content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8549/'
+        content: ''
+        // content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8549/'
     },
     1: {
         title: '韩璐璐',
         subtitle: '室内设计师',
         description: '2012, the world\'s most authoritative Awwwards selected 365 global best CSS website',
-        content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8548/'
+        content: ''
+        // content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8548/'
     },
     2: {
         title: '胡如珊',
         subtitle: '资深室内设计师',
         description: 'The world\'s leading electronic design journal NEWWEBPICK recommended the designer',
-        content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8547/'
+        content: ''
+        // content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8547/'
     },
     3: {
         title: '连君曼',
         subtitle: '室内设计师',
         description: '设计界的“设计鬼才”从题材到表现方式都引起了极大的福布斯中国最具潜力 ...',
-        content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8665/'
+        content: ''
+        // content: 'http://mo004_1836.mo4.line1.jsmo.xin/team/post/8665/'
     },
 }
